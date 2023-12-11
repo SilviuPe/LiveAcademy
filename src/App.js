11122eed42f9f133eba83a977d3ce9bd116c616c
SilviuPe <silviupetrea60@gmail.com>
2023-12-11 03:40:53 -0800

    Create a route for register page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { StartPage } from './containers';
+import { StartPage, Register } from './containers';
 
 const App = () => {
   return (
@@ -9,6 +9,7 @@ const App = () => {
         <BrowserRouter>
             <Routes>
                 <Route exact path = '/' element = {<StartPage/>} />
+                <Route path = '/register' element = {<Register/>} />
             </Routes>
         </BrowserRouter>
     </div>
