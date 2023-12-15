8bc08ef1c4bdafb654054d23c6eda3c055e25a2a
SilviuPe <silviupetrea60@gmail.com>
2023-12-15 03:33:25 -0800

    POST request to the server for creating a new account.

--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Register.css';
 
@@ -7,32 +7,79 @@ import mail from '../../assets/mail.png';
 import passwd from '../../assets/padlock.png';
 import { useNavigate } from 'react-router-dom';
 
-const RegisterField = ({icon,text,type}) => {
+
+
+const RegisterField = ({icon,text,type,callback}) => {
     return (
         <li className='LiveAcademy_field'>
             <img src = {icon}/>
-            <input type = {type} placeholder={text}/>
+            <input type = {type} placeholder={text} onChange = {callback}/>
         </li>
     );
 }
 const Register = () => {
+
   const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    username : '',
+    mail : '',
+    password : ''
+  })
+
+
   const handleLoginButton = () => {
     navigate('/login');
   }
   const handleHomeButton = () => {
     navigate('/');
   }
+
+  const SendRegisterRequest = () => {
+    let json_data = {
+      method : 'POST',
+      headers : {
+        'Content-Type' : 'application/json',
+      },
+      body : JSON.stringify(credentials)
+    }
+
+    fetch('http://localhost:3001/register', json_data)
+    .then(
+      response => {
+        console.log(response);
+      }
+    )
+  }
+
   return (
     <div className='LiveAcademy_register_container'>
         <div className='LiveAcademy_register_content'>
              <h1>Register</h1>
              <ul className='LiveAcademy_register_fields'>
-                <RegisterField icon = {user} text = "Username" type = "text"/>
-                <RegisterField icon = {mail} text = "Email" type = "email"/>
-                <RegisterField icon = {passwd} text = "Password" type = "password"/>
+                <RegisterField icon = {user} text = "Username" type = "text" callback = {(event) => {
+                  setCredentials({
+                    'username':event.target.value,
+                    'mail': credentials['mail'],
+                    'password' : credentials['password']
+                  })
+                  console.log(credentials);
+                }}/>
+                <RegisterField icon = {mail} text = "Email" type = "mail" callback = {(event) => {
+                  setCredentials({
+                    'username':credentials['username'],
+                    'mail': event.target.value,
+                    'password' : credentials['password']
+                  })
+                }}/>
+                <RegisterField icon = {passwd} text = "Password" type = "password" callback = {(event) => {
+                  setCredentials({
+                    'username':credentials['username'],
+                    'mail': credentials['mail'],
+                    'password' : event.target.value
+                  })
+                }}/>
              </ul>
-             <button>Submit</button>
+             <button onClick={SendRegisterRequest}>Submit</button>
 
              <p onClick = {handleLoginButton}>Already have an account? <span>Login</span></p>
              <p onClick = {handleHomeButton}> Want to go back to home page?</p>
