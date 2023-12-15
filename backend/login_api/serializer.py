9200bca717c0bc4b47771676b1af84bd5553a126
SilviuPe <silviupetrea60@gmail.com>
2023-12-15 07:43:09 -0800

    Adding validate function for username and email to check if someone else already use the same credentials

--- backend/login_api/serializer.py
+++ backend/login_api/serializer.py
@@ -4,7 +4,18 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta: 
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('username', 'email', 'password')
+
+    def validate_username(self,value):
+        if User.objects.filter(username=value).exists():
+            return serializers.ValidationError('Username already used')
+        return value
+    
+    def validate_email(self,value):
+        if User.objects.filter(email=value).exists():
+            return serializers.ValidationError('Email already used')
+        return value
+    
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
