50515b2f0ba797b368d7d3119ab528d19cc329ff
Silviu <silviupetrea60@gmail.com>
2023-12-22 04:10:57 -0800

    Added auhor for Course model.

--- backend/login_api/models.py
+++ backend/login_api/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 import random, string 
+from django.contrib.auth.models import User
 # Create your models here.
 
 def generate_random_id():
     code = ''.join(random.choice(string.digits + string.ascii_letters) for i in range(20)) # initiate code 
-    
+
     # Check if there are existing objects with that id 
     course_ = Course.objects.filter(id = code).exists()
     chart_ = Chart.objects.filter(id = code).exists()
@@ -12,13 +13,13 @@ def generate_random_id():
     # If yes, change the code till a new one is generated
     while course_ or chart_ or lecture_:
         code = ''.join(random.choice(string.digits + string.ascii_letters) for i in range(20))
-    return code 
+    return code
 
 class Course(models.Model):
     id = models.CharField(max_length=20, primary_key = True, default = generate_random_id, editable = False, unique = True)
     title = models.CharField(max_length=20, default = '')
     created = models.DateTimeField(auto_now_add=True)
-
+    author = models.ForeignKey(User,on_delete=models.CASCADE, default = None)
 
 class Chart(models.Model):
     id = models.CharField(max_length=20, primary_key = True, default = generate_random_id, editable = False, unique = True)
