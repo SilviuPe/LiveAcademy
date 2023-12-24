9df82956a03b91cfdb0e66276d73ed67c078e8c1
SilviuPe <silviupetrea60@gmail.com>
2023-12-24 09:19:13 -0800

    Export 'CourseCreator.jsx' file.

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,4 @@
 export {default as StartPage} from './StartPage/StartPage.jsx';
 export {default as Register} from './Register/Register.jsx';
 export {default as Login} from './Login/Login.jsx';
+export {default as CourseCreator} from './Create Course/CourseCreator.jsx';
