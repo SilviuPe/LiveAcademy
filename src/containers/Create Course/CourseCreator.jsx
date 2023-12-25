f135b9196a3c6781051fbc38f0378f5589e43282
SilviuPe <silviupetrea60@gmail.com>
2023-12-25 15:13:35 -0800

    Added edit icon for the existing chapters | System for adding new chapters done

--- src/containers/Create Course/CourseCreator.jsx
+++ src/containers/Create Course/CourseCreator.jsx
@@ -6,21 +6,18 @@ import { createChapter, openChapterInput } from './courseCreator.js';
 import { Header } from '../../components';
 
 import add from '../../assets/add.png';
+import edit from '../../assets/edit.png';
 
 
 const NewChapter = ({title}) => {
     return (
         <div className='LiveAcademy_course_chapter'>
             <h1>{title}</h1>
+            <div className='edit_img_holder'>
+                <img src = {edit}/>
+            </div>
         </div>
     );
-}
-export const Chart = ({title}) => {
-    return (
-        <div className='LiveAcademy_course_chapter'>
-            <h1>{title}</h1>
-        </div>
-    )
 }
 
 
@@ -43,6 +40,26 @@ const CourseCreator = () => {
             return newStructure;
         })
     }
+
+    // Add new chapter 
+    function addNewChapter(title) {
+        setChartTitleOpen(!chartTitleOpen)
+        setCourseStructure(() => {
+            let existing_chapters = courseStructure['Chapters'];
+            let newCourseStructure = {
+                ...courseStructure,
+                Chapters: {
+                    ...existing_chapters,
+                    [title]: {
+                        lectures : {
+
+                        }
+                    }
+                }
+            }
+            return newCourseStructure;
+        })
+    }
     return (
     <div className='LiveAcademy_createCourse_container'>
         <Header/>
@@ -50,7 +67,7 @@ const CourseCreator = () => {
             <div className='LiveAcademy_content-course_title'>
                 {
                     courseStructure.CourseTitle.length > 0
-                    ? <h1 title = 'Edit Title'>{courseStructure.CourseTitle}</h1>
+                    ? <h1 title = 'Edit Title' onClick = {()=> {updateCourseTitle('')}}>{courseStructure.CourseTitle}</h1>
                     : <input id = "LiveAcademy_input_course_title" placeholder='Title of the course'
                       onKeyDown={(event) => {
                         if(event.key === 'Enter') {
@@ -59,28 +76,32 @@ const CourseCreator = () => {
                       }}></input>
                 }
             </div>
-            
-            <h1 style={{color:'#fff'}}>{JSON.stringify(courseStructure)}</h1>
+     
             
             <div id='LiveAcademy_course_template'>
                 <div id='Add-New-Chapter'>
                     {
-                        Object.keys(courseStructure).map(key => {
-                            console.log(key,courseStructure[key]);
-                            return <></>
+                        Object.keys(courseStructure['Chapters']).map(key => {
+                            return <NewChapter title = {key}/>
                         })
                     }
-                    {
-                        chartTitleOpen
-                        ? <input onKeyDown = {(event) => {
-                            return (
-                                event.key === 'Enter' && event.target.value.length > 0
-                                ? <></>
-                                : <></>
-                            )
-                        }}placeholder='Title of the chapter'></input>
-                        : <img src = {add} onClick={() => {setChartTitleOpen(!chartTitleOpen)}}/>
-                    }
+                        <div id='new_chapter_input'>
+                        {
+                            chartTitleOpen
+                            ? <input onKeyDown = {(event) => {
+                                return (
+                                    event.key === 'Enter' && event.target.value.length > 0
+                                    ? addNewChapter(event.target.value) 
+                                    : <></>
+                                )
+                            }}placeholder='Title of the chapter'></input>
+                            : <img src = {add} onClick={() => {
+                                if(courseStructure.CourseTitle.length > 0)
+                                setChartTitleOpen(!chartTitleOpen)
+                            }}/>
+
+                        }
+                        </div>
                 </div>
             </div>
         </main>
