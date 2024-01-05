73585a3a3d9201d755571625d9cd098d5572bf6f
Silviu <silviupetrea60@gmail.com>
2024-01-05 01:48:47 -0800

    Added new view when for saving a new course.

--- backend/login_api/views.py
+++ backend/login_api/views.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework import status
-from django.contrib.auth import login, authenticate
-
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as log
 from django.contrib.auth.models import User
-
+from .models import Course, Chapter, Lecture
 from .serializer import UserSerializer, LoginSerializer
 
 
@@ -28,8 +29,68 @@ class login(APIView):
         if serializer.is_valid():
             username = serializer.validated_data['username']
             password = serializer.validated_data['password']
-            user = User.objects.filter(username=username,password=password).exists()
-            if user:
-                login(request=request,user=user)
-                return Response({"Message":"Login succesfully"},status=status.HTTP_200_OK)
+            user = User.objects.filter(username=username,password=password)[0]
+            if user != None: 
+                return Response({"Message":"Login succesfully", "username" : user.username },status=status.HTTP_200_OK)
         return Response({"Error":"Login failed"},status=status.HTTP_400_BAD_REQUEST)
+
+
+class NewCourseRequest(APIView):
+
+    def post(self,request):
+        data = dict(request.data)
+        status_code = status.HTTP_400_BAD_REQUEST
+        filterMessage = self.filterCourse(data)
+        if "Message" in filterMessage:
+            newCourse = self.createNewCourse(dict(request.data))
+            newCourse.save()
+            self.createNewChapters(dict(request.data)['Chapters'],newCourse.id)
+            status_code = status.HTTP_201_CREATED
+        return Response(filterMessage, status= status_code)
+        
+
+
+
+    def createNewCourse(self,courseStructure):
+        title = courseStructure['CourseTitle']
+        user = User.objects.filter(username=courseStructure['Author'])[0]
+        return Course(title = title, author = user)
+    
+    def createNewChapters(self,ChaptersList,courseID):
+        for chapter in ChaptersList:
+            lecture_count = len(list(ChaptersList[chapter]['lectures']))
+            newChapter = Chapter(title = chapter, 
+                                lecture_count = lecture_count, 
+                                course = Course.objects.filter(id = courseID)[0])
+            newChapter.save()
+            self.createNewLectures(ChaptersList[chapter]['lectures'],newChapter.id)
+
+
+    def createNewLectures(self,LectureList,chapterID):
+        for lecture in LectureList:
+            newLecture = Lecture(title = lecture, 
+                                chapter = Chapter.objects.filter(id = chapterID)[0])
+            newLecture.save()
+
+
+    def filterCourse(self,courseStructure):
+        title = courseStructure['CourseTitle']
+        chapter_list = courseStructure['Chapters']
+        user = User.objects.filter(username = courseStructure['Author']).exists()
+        if len(title) <= 0:
+            return {"Error" : "Title of the course is missing!"} 
+        if not user:
+            return {"Error" : "An user with that name doesn't exist!"} 
+        if len(chapter_list) <= 0:
+            return {"Error" : "Chapters missing!"}
+        for chapter in chapter_list:
+            lectures = chapter_list[chapter]
+            if len(lectures['lectures']) <= 0:
+                return {"Error" : "Lectures missing!"}
+        
+        return {"Message" : "Course succesfully created!"}
+
+class test(APIView):
+    def get(self,request):
+        print(type(request.user.username),request.user.username)
+        return Response('asdsad')
