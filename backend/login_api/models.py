1652d4f376fd7bdefdd20566e61f77c1f33902ac
Silviu <silviupetrea60@gmail.com>
2024-01-05 01:45:43 -0800

    Rename 'Chart' to 'Chapter'.

--- backend/login_api/models.py
+++ backend/login_api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import random, string 
 from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 
 def generate_random_id():
@@ -8,7 +9,7 @@ def generate_random_id():
 
     # Check if there are existing objects with that id 
     course_ = Course.objects.filter(id = code).exists()
-    chart_ = Chart.objects.filter(id = code).exists()
+    chart_ = Chapter.objects.filter(id = code).exists()
     lecture_ = Lecture.objects.filter(id = code).exists()
     # If yes, change the code till a new one is generated
     while course_ or chart_ or lecture_:
@@ -21,7 +22,7 @@ class Course(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE, default = None)
 
-class Chart(models.Model):
+class Chapter(models.Model):
     id = models.CharField(max_length=20, primary_key = True, default = generate_random_id, editable = False, unique = True)
     title = models.CharField(max_length=20, default = '')
     lecture_count = models.IntegerField()
@@ -32,6 +33,6 @@ class Lecture(models.Model):
     id = models.CharField(max_length=20, primary_key = True, default = generate_random_id, editable = False, unique = True)
     title = models.CharField(max_length=40, default = '')
     path = models.CharField(max_length=100,default = '')
-    duration = models.DurationField()
-    chart = models.ForeignKey(Chart,on_delete=models.CASCADE)
+    duration = models.DurationField(default = timezone.timedelta(seconds=0))
+    chapter = models.ForeignKey(Chapter,on_delete=models.CASCADE)
     document_type = models.CharField(max_length=20,default = 'html_document')
