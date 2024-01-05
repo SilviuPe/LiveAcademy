960fa4c17296f6a0a8e0dc19dbb7fcca34141c37
Silviu <silviupetrea60@gmail.com>
2024-01-05 01:47:17 -0800

    Added path for creating a new course and a separate path for testing.

--- backend/login_api/urls.py
+++ backend/login_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-from .views import register, login
+from .views import register, login, NewCourseRequest, test
 
 urlpatterns = [
     path('register', register.as_view()),
-    path('login', login.as_view())
+    path('login', login.as_view()),
+    path('course_request', NewCourseRequest.as_view()),
+    path('test',test.as_view())
 ]
