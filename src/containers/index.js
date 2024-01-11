f2521123fd99cf758515da9b3c6d0314161d9657
Silviu <silviupetrea60@gmail.com>
2024-01-11 05:42:47 -0800

    Export 'CreateContent.jsx' file from 'Create Content' folder

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,3 +2,4 @@ export {default as StartPage} from './StartPage/StartPage.jsx';
 export {default as Register} from './Register/Register.jsx';
 export {default as Login} from './Login/Login.jsx';
 export {default as CourseCreator} from './Create Course/CourseCreator.jsx';
+export {default as CreateContent} from './Create Content/CreateContent.jsx';
