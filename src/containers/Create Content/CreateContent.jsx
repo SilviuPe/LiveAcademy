69f4ca4f1285c1266ae1018a9a8e85f2282028c0
Silviu <silviupetrea60@gmail.com>
2024-01-15 12:48:39 -0800

    Function for requesting the lecture. Still working on it!

--- src/containers/Create Content/CreateContent.jsx
+++ src/containers/Create Content/CreateContent.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import {Header} from '../../components';
 import { useParams } from 'react-router-dom';
 
+
 const CreateContent = () => {
     const { lectureID } = useParams();
-    console.log(lectureID);
+    const lecture = fetch(`http://localhost:3001/request_lecture?id=${lectureID}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+    console.log(lecture);
+
     return(
         <div className='LiveAcademy_create_content-container'>
             <Header/>
         </div>
     );
-}
+} 
 
-export default CreateContent;
+export default CreateContent; 
